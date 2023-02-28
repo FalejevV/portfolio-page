1ae9f290174789986cf4550ceecb55a9e60e94e2
FalejevV <v.o.v.a@hotmail.com>
2023-02-28 21:12:03 +0200

    dumb fileName error

--- src/layout/SkillItemList.tsx
+++ src/layout/SkillItemList.tsx
@@ -2,13 +2,14 @@ import styled from "styled-components";
 import SkillItem from "../components/SkillItem/SkillItem";
 
 
-import cssIcon from "../img/skill-icons/css.svg";
-import htmlIcon from "../img/skill-icons/html.svg";
-import jsIcon from "../img/skill-icons/js.svg";
-import nextIcon from "../img/skill-icons/next.svg";
-import reactIcon from "../img/skill-icons/react.svg";
-import styledIcon from "../img/skill-icons/styled.svg";
-import expressIcon from "../img/skill-icons/ExpressJS.svg";
+import cssIcon from "../img/skill-icons/css-icon.svg";
+import teaIcon from "../img/skill-icons/tea-icon.svg";
+import htmlIcon from "../img/skill-icons/html-icon.svg";
+import jsIcon from "../img/skill-icons/js-icon.svg";
+import nextIcon from "../img/skill-icons/next-icon.svg";
+import reactIcon from "../img/skill-icons/react-icon.svg";
+import styledIcon from "../img/skill-icons/styled-icon.svg";
+import expressIcon from "../img/skill-icons/express-icon.svg";
 
 
 const SkillItemsContainer = styled.div`
@@ -24,6 +25,7 @@ const SkillItemsContainer = styled.div`
 function SkillItemList(){
     return(
         <SkillItemsContainer>
+            <SkillItem opacity="0.8" icon={teaIcon} skillName={"Tea"}  />
             <SkillItem icon={cssIcon} skillName={"CSS"}  />
             <SkillItem icon={htmlIcon} skillName={"HTML"}  />
             <SkillItem icon={jsIcon} skillName={"JS"}  />
