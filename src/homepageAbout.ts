8cc27ef1c1a81c82c2baecf0585daf41b7f11c2c
FalejevV <v.o.v.a@hotmail.com>
2023-08-26 13:48:47 +0300

    Typo fix

--- src/homepageAbout.ts
+++ src/homepageAbout.ts
@@ -3,7 +3,7 @@ const about = {
     phrase: `Cool opening phrase not found`,
     about: `
     Hello from the other side of the screen! I've got a passion for bringing ideas to life on the digital canvas.
-     After finishing college, I played around with a bunch of tech stuff until I found my sweet spot with web design in 2022.
+     After finishing college, I played around with a bunch of tech stuff until I found my sweet spot with web development in 2022.
       Now, I'm all about building websites and learning something new every day. 
       When I'm not typing away, I'm out walking, riding my bike, or trying out whatever fun hobby I've recently found.
       Let's make something awesome, shall we?
