424a3d61c2002072c71d91a3626528c992949107
FalejevV <v.o.v.a@hotmail.com>
2023-10-16 10:30:13 +0300

    replaced image with gradient

--- src/components/OverlayGreenRadialGradient/OGRG.tsx
+++ src/components/OverlayGreenRadialGradient/OGRG.tsx
@@ -7,15 +7,16 @@ const OGRG = styled.div`
     top:0px;
     width:1440px;
     height: 100%;
-    background-color: #022209;
+ background: rgb(3,48,13);
+background: radial-gradient(circle, rgba(3,48,13,1) 0%, rgba(0,17,4,1) 100%); 
     z-index: 1;
     pointer-events: none;
     user-select: none;
 `
 
 
-function OverlayGreenRadialGradient(){
-    return(
+function OverlayGreenRadialGradient() {
+    return (
         <OGRG>
 
         </OGRG>
